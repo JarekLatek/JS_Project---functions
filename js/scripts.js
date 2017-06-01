a7b57cb5d456fa62e87e6e6d4794e4486cae2594
Jarek Latek <jareklatek@gmail.com>
2017-06-01 12:19:39 +0200

    Change code scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-var triangle1Area = getTriangleArea(10, 15);
-	triangle2Area = getTriangleArea(10, 6);
-	triangle3Area = getTriangleArea(12,13);
-
 function getTriangleArea(a, h) {
-
-	var triangleArea = a*h/2;
 	
 	if ( (a <= 0) || (h <= 0) ) {
 		console.log('Nieprawidłowe dane.');
 	} else {
 		console.log('Dane prawidłowe - obliczam pola trójkątów.');
 	}
-	return(triangleArea);
+	return(a*h/2);
 }
 
+var triangle1Area = getTriangleArea(10, 15),
+	triangle2Area = getTriangleArea(10, 6),
+	triangle3Area = getTriangleArea(12,13);
+	
 console.log( 'Pole trójkąta 1 wynosi: ' + triangle1Area );
 console.log( 'Pole trójkąta 2 wynosi: ' + triangle2Area );
 console.log( 'Pole trójkąta 3 wynosi: ' + triangle3Area );
